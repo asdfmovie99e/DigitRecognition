1df390ae774c234a306bac4744d64129f4ad6de2
asdfmovie99e <aaa59@hotmail.de>
2018-10-01 13:27:39 +0200

    .

--- src/main/Main.java
+++ src/main/Main.java
@@ -18,6 +18,7 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
-        launch(args);
+        System.out.println("Hello world");
+        //launch(args);
     }
 }
