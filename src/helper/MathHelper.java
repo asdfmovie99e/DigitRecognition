8a351dc7fcbe060cbb5cefa30528da98be8fab6e
JensK <aaa59@hotmail.de>
2018-10-03 09:17:23 +0200

    images will be stored in appdata now

---
+++ src/helper/MathHelper.java
@@ -0,0 +1,26 @@
+package helper;
+
+        import java.util.HashMap;
+
+public class MathHelper {
+    private static HashMap<Integer, Float> sigmoidMap;
+
+
+    public static void start(){
+        sigmoidMap = new HashMap<Integer, Float>();
+        for(int i = -60; i <= 60; i += 1){
+            sigmoidMap.put(i,  (float) (1 / (1 + Math.pow(Math.E, -(i/10)))));
+        }
+        int a = 1+1;
+    }
+
+
+
+    public static float sigmoidApprox(float input){
+
+        if(input < - 6) return 0f;
+        if(input > 6) return 1f;
+        return sigmoidMap.get(Math.round(input * 10));
+    }
+
+}
