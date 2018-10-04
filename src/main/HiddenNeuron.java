0efa49ff720a73971a2cf31d6f65383a7649429f
asdfmovie99e <aaa59@hotmail.de>
2018-10-04 10:51:58 +0200

    irgenwas

--- src/main/HiddenNeuron.java
+++ src/main/HiddenNeuron.java
@@ -3,4 +3,14 @@ package main;
 
 class HiddenNeuron extends Neuron {
 
+
+    public void adjustWeights(float targetWeight){
+        for(int i = 0; i < previousNeurons.length; i++)
+        {
+            float delta = learningFactor * previousNeurons[i].getOutputValue() * (targetWeight - getOutputValue());
+            float oldWeight = weightMap.get(i);
+            weightMap.remove(i);
+            weightMap.put(i, oldWeight + delta);
+        }
+
 }
--- src/main/OutputNeuron.java
+++ src/main/OutputNeuron.java
@@ -2,6 +2,18 @@ package main;
 
 class OutputNeuron extends Neuron{
 
+
     @Override
     public void setNextNeurons(Neuron[] nextNeurons){}// not used because output neurons dont have a next layer
+
+    public void adjustWeights(float targetWeight){
+        for(int i = 0; i < previousNeurons.length; i++)
+        {
+            float delta = learningFactor * previousNeurons[i].getOutputValue() * (targetWeight - getOutputValue());
+            float oldWeight = weightMap.get(i);
+            weightMap.remove(i);
+            weightMap.put(i, oldWeight + delta);
+        }
+
+    }
 }
